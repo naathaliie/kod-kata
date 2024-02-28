0703ae88b8d1e328b346332b8646db34d0769fe0
naathaliie <nathalielarsson@live.se>
2024-02-28 14:02:33 +0100

    övning 5, klar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './Header'
+import RepeatText from './RepeatText'
 
 function App() {
 /* const headerText = "Välkommen till min sida"; */
@@ -8,6 +9,7 @@ function App() {
     <>
       <h1>liten kod-kata react</h1>
       <Header headerText="Steg 4, klart"/>
+      <RepeatText count={7} text="En liten båt blir ofta våt om magen när det stänker"/>
     </>
   )
 }
---
+++ src/RepeatText.jsx
@@ -0,0 +1,14 @@
+/* Skapa en ny komponent RepeatText som tar emot en prop 
+count som är en siffra och text som är en sträng.
+Komponenten innehåller sedan så många p-taggar som count 
+är och texten inne i p-taggarna är det som finns i propen text. */
+
+const RepeatText = ({ count, text }) => {
+  const paragraph = Array.from({length: count}, (index) => (
+    <p key={index}>{text}</p>
+  ))
+
+  return <div>{paragraph}</div>
+};
+
+export default RepeatText;
