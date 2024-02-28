688b09ae56a99daae35c33ee62609b30911979e8
naathaliie <nathalielarsson@live.se>
2024-02-28 13:36:41 +0100

    steg 4 klart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './Header'
 
 function App() {
+/* const headerText = "Välkommen till min sida"; */
 
   return (
     <>
       <h1>liten kod-kata react</h1>
+      <Header headerText="Steg 4, klart"/>
     </>
   )
 }
