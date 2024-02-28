339b8f89d0c746bb1830b3af75705bfb76be473a
naathaliie <nathalielarsson@live.se>
2024-02-28 14:27:09 +0100

    steg 6, el klaro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './Header'
+import List from './List'
 import RepeatText from './RepeatText'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <h1>liten kod-kata react</h1>
       <Header headerText="Steg 4, klart"/>
       <RepeatText count={7} text="En liten båt blir ofta våt om magen när det stänker"/>
+      <List animalArray={['ko', 'häst', 'hund', 'giraff']}/>
     </>
   )
 }
