26cd9441706441f8bd7b10b543223ca2e2f91c6d
naathaliie <nathalielarsson@live.se>
2024-03-04 09:03:44 +0100

    frames for each thing

--- src/DropDown.jsx
+++ src/DropDown.jsx
@@ -1,5 +1,11 @@
 import "../src/App.css";
 
+/* Skapa en ny komponent DropDown som har en ruta 
+för att visa text och en dropdown. När man väljer ngt 
+val i dropdownen så ska det visas i textrutan.
+ Komponenten kan ta emot en array med fordon (strängar) 
+ som läggs till i dropdownen. */
+
 const DropDown = () => {
   return (
     <div className="dropDownDiv">
