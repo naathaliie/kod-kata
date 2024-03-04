978e3e9bb6973349176e82003bad38c046426966
naathaliie <nathalielarsson@live.se>
2024-03-04 12:58:27 +0100

    DropDown added

--- src/DropDown.jsx
+++ src/DropDown.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../src/App.css";
 
 /* Skapa en ny komponent DropDown som har en ruta 
@@ -7,10 +8,30 @@ val i dropdownen så ska det visas i textrutan.
  som läggs till i dropdownen. */
 
 const DropDown = () => {
+//En array med hundraser
+const [dog, setDog] = useState(null);
+
+const cuteDogs = ["Dalmatiner", "Chow Chow", "Papillon", "Cocker spaniel", "Boer boel"];
+const handleOnClick = (e) => {
+  console.log(e.target.value);
+  setDog(e.target.value);
+};
+
   return (
     <div className="dropDownDiv">
-      {" "}
-      <h1>DropDown</h1>
+      <h1>Dropdown</h1>
+      <div className="theDropDown">
+        <label htmlFor="dropDown">Söta hundraser</label>
+        <select name="dogs" id="dropDown" onClick={handleOnClick}>
+          <option value="">Söta voffsingar</option>
+          {cuteDogs.map((dog, index) => {
+              return <option key={index} value={`${dog}`}>{dog}</option>
+          })}
+        </select>
+      </div>
+      <div className="DropDownBox">
+        <h2>{dog}</h2>
+      </div>
     </div>
   );
 };
